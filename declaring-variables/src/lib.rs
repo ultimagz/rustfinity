b7586bc03dccb45517b7790ec8ad16618ff49e30
Pitsanu Kittipittayakorn <pitsanu.kittipittayakorn@scbtechx.io>
2024-11-09 16:04:08 +0700

    Submit challenge declaring-variables

---
+++ declaring-variables/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn calculate_area() -> u32 {
+    // TODO: Implement the function here
+    // Declare width and height as immutable variables
+    let width = 10;
+    let height = 5;
+    // Calculate the area of the rectangle
+    let area = width * height;
+    // Return the calculated area
+    return area;
+}
