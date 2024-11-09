ad123ce92cfe7724080e359e35266063e2304d37
Pitsanu Kittipittayakorn <pitsanu.kittipittayakorn@scbtechx.io>
2024-11-09 16:21:47 +0700

    Submit challenge mutable-variables

---
+++ beginner/mutable-variables/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn mutating_variables() -> &'static str {
+    // 1. Declare a mutable variable `text` with value "hello"
+    let mut text = "hello";
+    // 2. Reassign the value of `text` to a new value
+    text = "text";
+    // 3. Return the value of `text`
+    return text;
+}
